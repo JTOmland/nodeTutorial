66d54a12d330fb6d67414f5aa8bba53fe58274a8
Jeff Omland <jeff.omland@gmail.com>
2017-06-15 20:57:06 -0500

    added ecosystems.config.js for deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'ScratchPad',
+    script: './app.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-54-227-228-8.compute-1.amazonaws.com',
+      key: '~/.ssh/ApostateNode.pem',
+      ref: 'origin/master',
+      repo: 'https://github.com/JTOmland/nodeTutorial.git',
+      path: '/home/ubuntu/Apostate',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
