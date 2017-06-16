716df9ee300fb4bc99d4a92e59ad1b0cf0334881
Jeff Omland <jeff.omland@gmail.com>
2017-06-16 06:48:54 -0500

    changed ecosystems path to match actual path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       key: '~/.ssh/ApostateNode.pem',
       ref: 'origin/master',
       repo: 'https://github.com/JTOmland/nodeTutorial.git',
-      path: '/home/ubuntu/Apostate',
+      path: '/home/ubuntu/ScratchPad',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
